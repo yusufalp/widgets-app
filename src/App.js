7c1338ecef87afdf9b4ccf0be7c81a68f6a309c6
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-22 13:58:11 -0800

    Add Accordion menu items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import './App.css';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'A JavaScript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'It is popular in front-end engineering'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'By creating components'
+  },
+]
 
 function App() {
   return (
@@ -6,6 +22,9 @@ function App() {
       <header className="App-header">
         <h1>Widgets App</h1>
       </header>
+      <div className='accordion'>
+        <Accordion items={items} />
+      </div>
     </div>
   );
 }
