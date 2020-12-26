5e795dcd7674a7bc7a104b079778a5d3f410a21d
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-26 11:46:14 -0800

    Add a timeout for search term change

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import './Search.css';
 import axios from 'axios';
-//https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=SEARCH_TERM
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      await axios.get(`https://en.wikipedia.org/w/api.php`, {
+      const { data } = await axios.get(`https://en.wikipedia.org/w/api.php`, {
         params: {
           action: 'query',
           list: 'search',
@@ -16,26 +17,61 @@ const Search = () => {
           srsearch: searchTerm
         },
       });
-    }
-    search();
+      setResults(data.query.search);
+    };
+
+    const timeoutId = setTimeout(() => {
+      if (searchTerm) {
+        search();
+      }
+    }, 1000);
+
+    return (() => {
+      clearTimeout(timeoutId);
+    })
 
   }, [searchTerm]);
+
+  const renderedResults = results.map(result => {
+    return (
+      <div key={result.pageid} className='group'>
+        <div>
+          <h2>{result.title}</h2>
+          <p dangerouslySetInnerHTML={{ __html: result.snippet }}></p>
+        </div>
+        <a
+          href={`https://en.wikipedia.org/wiki?curid=${result.pageid}`}
+          className='button'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Go to page
+        </a>
+      </div>
+    )
+  })
 
   const searchTermHandler = (term) => {
     setSearchTerm(term);
   }
-  console.log(searchTerm);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
   return (
-    <form>
-      <label htmlFor='search-term'>Search:</label>
-      <input
-        id='search-term'
-        name='search-term'
-        type='text'
-        value={searchTerm}
-        onChange={e => searchTermHandler(e.target.value)}
-      />
-    </form>
+    <div>
+      <form onSubmit={e => submitHandler(e)}>
+        <label htmlFor='search-term'>Search:</label>
+        <input
+          id='search-term'
+          name='search-term'
+          type='text'
+          value={searchTerm}
+          onChange={e => searchTermHandler(e.target.value)}
+        />
+      </form>
+      {renderedResults}
+    </div>
   );
 }
 
